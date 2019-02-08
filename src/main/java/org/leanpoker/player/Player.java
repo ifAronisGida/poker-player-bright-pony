3c36e160003dd4945501070ca1ddfec26692e52e
Aron Gida <ifAronisGida@github.com>
2019-02-08 10:40:01 +0100

    lets play 1000!

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "Pony 1.0.0";
 
     public static int betRequest(JsonElement request) {
-        return 10;
+        return 1000;
     }
 
     public static void showdown(JsonElement game) {
