522a01f770479aa6a0394b20e0cde25e052fc724
Doram13 <1991doram@gmail.com>
2019-02-08 11:26:14 +0100

    check

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,7 +1,11 @@
 package org.leanpoker.player;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import org.leanpoker.player.game.Bot;
+import org.leanpoker.player.game.GameState;
 
+import java.util.List;
 import java.util.Map;
 
 public class Player {
@@ -9,7 +13,13 @@ public class Player {
     static final String VERSION = "Pony 1.0.0";
 
     public static int betRequest(JsonElement request) {
-        return 50;
+        Gson gson = new Gson();
+        GameState gameState = gson.fromJson(request, GameState.class);
+        int current_buy_in = gameState.getCurrent_buy_in();
+        Bot our_player  = gameState.getPlayerByName("Bright Ponies");
+        List<Bot> players  = gameState.getPlayers();
+        our_bet = current_buy_in - our_player.getBet();
+        return our_bet;
     }
 
     public static void showdown(JsonElement game) {
