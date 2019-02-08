70527985ca258fcbb0980dae8ae21ce5a16318dd
Aron Gida <ifAronisGida@github.com>
2019-02-08 12:04:17 +0100

    all in fix

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -18,14 +18,14 @@ public class Player {
         int current_buy_in = gameState.getCurrent_buy_in();
         Bot our_player  = gameState.getPlayerByName("Bright Pony");
         List<Bot> players  = gameState.getPlayers();
-        int our_bet = current_buy_in - our_player.getBet();
-        if (current_buy_in > 900 && gameState.getCommunity_cards().isEmpty()) {
-            our_bet = 0;
-        }
         String rankOfFirstCard = our_player.getHole_cards().get(0).getRank();
         String rankOfSecondCard = our_player.getHole_cards().get(1).getRank();
-        if (rankOfFirstCard.equals(rankOfSecondCard)){
-            return our_bet + 50;
+        int our_bet = current_buy_in - our_player.getBet();
+        if (current_buy_in > 900 && gameState.getCommunity_cards().size() < 4) {
+            our_bet = 0;
+        }
+        else if (rankOfFirstCard.equals(rankOfSecondCard)){
+            our_bet += 50;
         }
         return our_bet;
     }
