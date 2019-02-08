57665fa97cc3076c8e586f85c3a1a7ac1f6b5237
Aron Gida <ifAronisGida@github.com>
2019-02-08 11:28:12 +0100

    bugfix

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -16,9 +16,9 @@ public class Player {
         Gson gson = new Gson();
         GameState gameState = gson.fromJson(request, GameState.class);
         int current_buy_in = gameState.getCurrent_buy_in();
-        Bot our_player  = gameState.getPlayerByName("Bright Ponies");
+        Bot our_player  = gameState.getPlayerByName("Bright Pony");
         List<Bot> players  = gameState.getPlayers();
-        our_bet = current_buy_in - our_player.getBet();
+        int our_bet = current_buy_in - our_player.getBet();
         return our_bet;
     }
 
