2c8f058b2881295d7f8ef528aa2b93460c2de3f6
Aron Gida <ifAronisGida@github.com>
2019-02-08 10:26:38 +0100

    lets play!

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "Pony 1.0.0";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        return 1;
     }
 
     public static void showdown(JsonElement game) {
