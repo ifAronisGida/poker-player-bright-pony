8c4a9abf4875fa94d4f2006dc4884c917ce1098d
Aron Gida <ifAronisGida@github.com>
2019-02-08 13:05:19 +0100

    refactored eval card to getValue from card object

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,6 +3,7 @@ package org.leanpoker.player;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import org.leanpoker.player.game.Bot;
+import org.leanpoker.player.game.Card;
 import org.leanpoker.player.game.GameState;
 
 import java.util.List;
@@ -30,52 +31,7 @@ public class Player {
         return our_bet;
     }
 
-    public static int evalCard(String input) {
-        int toReturn;
-        switch (input) {
-            case "2":
-                toReturn = 2;
-                break;
-            case "3":
-                toReturn = 3;
-                break;
-            case "4":
-                toReturn = 4;
-            break;
-            case "5":
-                toReturn = 5;
-            break;
-            case "6":
-                toReturn = 6;
-            break;
-            case "7":
-                toReturn = 7;
-            break;
-            case "8":
-                toReturn = 8;
-            break;
-            case "9":
-                toReturn = 9;
-            break;
-            case "10":
-                toReturn = 10;
-            break;
-            case "J":
-                toReturn = 11;
-            break;
-            case "Q":
-                toReturn = 12;
-            break;
-            case "K":
-                toReturn = 13;
-            break;
-            case "A":
-                toReturn = 14;
-            break;
-            default: toReturn = 0;
-        }
-    return toReturn;
-    }
+
 
 
     public static void showdown(JsonElement game) {
--- src/main/java/org/leanpoker/player/game/Card.java
+++ src/main/java/org/leanpoker/player/game/Card.java
@@ -13,6 +13,53 @@ public class Card {
         return suit;
     }
 
+    public int getValue() {
+        int toReturn;
+        switch (rank) {
+            case "2":
+                toReturn = 2;
+                break;
+            case "3":
+                toReturn = 3;
+                break;
+            case "4":
+                toReturn = 4;
+                break;
+            case "5":
+                toReturn = 5;
+                break;
+            case "6":
+                toReturn = 6;
+                break;
+            case "7":
+                toReturn = 7;
+                break;
+            case "8":
+                toReturn = 8;
+                break;
+            case "9":
+                toReturn = 9;
+                break;
+            case "10":
+                toReturn = 10;
+                break;
+            case "J":
+                toReturn = 11;
+                break;
+            case "Q":
+                toReturn = 12;
+                break;
+            case "K":
+                toReturn = 13;
+                break;
+            case "A":
+                toReturn = 14;
+                break;
+            default: toReturn = 0;
+        }
+        return toReturn;
+    }
+
     @Override
     public String toString() {
         return "Card{" +
